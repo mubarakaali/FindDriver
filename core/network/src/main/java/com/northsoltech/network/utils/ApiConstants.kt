8266c02b0db1e6d3701993c1852754acde9759c2
Mubarak Ali <mubarakyvro@gmail.com>
2022-10-05 00:33:48 +0500

    changed base url

--- core/network/src/main/java/com/northsoltech/network/utils/ApiConstants.kt
+++ core/network/src/main/java/com/northsoltech/network/utils/ApiConstants.kt
@@ -2,7 +2,7 @@ package com.northsoltech.network.utils
 
 internal object ApiConstants {
 
-    const val BASE_URL = "https://pickmecarapp.herokuapp.com/"
+    const val BASE_URL = "https://herokuapp.com/test/"
     const val SIGNUP = "api/signup"
     const val SIGNIN = "api/login"
 }
